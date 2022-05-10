efc56513aafc9a5bca1161fa1b15411e3efd68ce
Anindita <reachanip@gmail.com>
2022-05-10 00:13:23 -0700

    submit example 4 & 5

--- src/labs_examples/fundamentals/labs/Exercise_04.java
+++ src/labs_examples/fundamentals/labs/Exercise_04.java
@@ -15,7 +15,27 @@ public class Exercise_04 {
         int i = 1;
         System.out.println("int i is: " + i);
 
-        // write your code below
+        byte j = 5;
+        System.out.println("byte j is: " + j);
+
+        long k = 56789;
+        System.out.println("long k is: " + k);
+
+        float l = 7.8f;
+        System.out.println("float l is: " + l);
+
+        double m = 234.987;
+        System.out.println("double m is: " + m);
+
+        char n = 'c';
+        System.out.println("char n is: " + n);
+
+        short o = 8;
+        System.out.println("short o is: " + o);
+
+        String p = "ani";
+        System.out.println("String p is: " + p);
+
 
     }
 
--- src/labs_examples/fundamentals/labs/Exercise_05.java
+++ src/labs_examples/fundamentals/labs/Exercise_05.java
@@ -13,15 +13,27 @@ public class Exercise_05 {
     public static void main(String[] args) {
 
         String str = "hello!";
-        // please declare an int variable below, and set it to the value of the length of "str"
+        int length = str.length();
+        System.out.println("the length of string is: " + str);
 
         String str2 = "hello";
-        // please initialize a boolean variable and test whether str is equal to str2
+        boolean equal = str.equalsIgnoreCase(str2);
+        System.out.println("str and str2 is equal: " + equal);
 
-        // please concatenate str & str2 and set the result to a new String variable below
+        String concat = str.concat(str2);
+        System.out.println("concatenate str & str2: " + concat);
 
-        // please demonstrate the use of any other method that is available to us in the String class
-        // for example, replace(), substring(), contains(), indexOf() etc
+        String replace = str.replace('!','?');
+        System.out.println("the new string is: " + replace);
+
+        String sub = str.substring(1,4);
+        System.out.println("the substring is: " + sub);
+
+        Boolean contain = str.contains("ll");
+        System.out.println("Is str contain ll: " + contain);
+
+        System.out.println("the index of str of ello: " + str.indexOf("ello"));
+
 
     }
 
